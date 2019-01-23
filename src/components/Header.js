7df35aa7c93b6eeba27bb3567da09c509eba587f
SalmanBiceps <thepunisher150@gmail.com>
2019-01-23 17:11:31 +0800

    Update Header.js
    changed the alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header>
             <img 
                 src="https://steemitimages.com/DQmVtTeesSnKmn5RwnsMytARstizA3mCMC9wYXbJQo4ZDiz/decent%20memes%20logo%20clear.png" 
-                alt="Meme Generator Logo"
+                alt="Meme Generator Image"
             />
             <p>Meme Generator </p>
         </header>
